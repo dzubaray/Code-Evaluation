d0a082f5d3a54e00dc101ce673ed2b04bbb9fa23
Dario Zubaray <dzubaray@gmail.com>
2021-11-18 15:41:14 -0300

    adding express-validator, checking if the trading pair is one of the valid option

--- src/routes/book.js
+++ src/routes/book.js
@@ -1,8 +1,21 @@
 const { Router } = require('express')
+const { param } = require('express-validator')
+const { validateFields } = require('../middlewares/validateFields')
 const { getBooks } = require('../controllers/book')
 
 const router = Router()
 
-router.get('/book/:symbol', getBooks)
+router.get('/book/:symbol',
+            [
+                param('symbol')
+                    .exists()
+                    .withMessage('The trading pair is Requiered')
+                    .isString()
+                    .withMessage('The trading pair must be a String')
+                    .isIn(['tBTCUSD', 'tETHUSD'])
+                    .withMessage('The trading pair must be either \'tBTCUSD\' or \'tETHUSD\''),
+                validateFields
+            ],
+            getBooks)
 
 module.exports = router
